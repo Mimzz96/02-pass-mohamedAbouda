66661bb4da28fd5a7ea59db9fac4405b2df44ea8
Mimzz96 <abouda.meed@gmail.com>
2021-03-29 20:42:04 +0200

    Created two interfaces (basic operations and advanced operations).

---
+++ src/main/java/AdvancedOperations.java
@@ -0,0 +1,15 @@
+public interface AdvancedOperations {
+    int square(int x);
+
+    int power(int x, int p);
+
+    double squareRoot(int x);
+
+    double root(int x, int r);
+
+    int absolute(int x);
+
+
+
+
+}
---
+++ src/main/java/BasicOperations.java
@@ -0,0 +1,14 @@
+public interface BasicOperations {
+    int add(int x, int y);
+
+    int substract(int x, int y);
+
+    int multiply(int x, int y);
+
+    double divide(int x, int y);
+
+
+
+
+
+}
